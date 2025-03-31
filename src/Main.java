5adbb3eed4f19d66ae8c60b50af7ed6c50357cde
frankfelipro_meli <frankfelipe.rojas@mercadolibre.com.co>
2025-03-31 10:22:05 -0500

    Fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Persona p1 = new Persona();
         Persona p2 = new Persona("Frank", 24, "123456789");
-        Persona p3 = new Persona("Felipe", 17, "123456789", 175.0, 78.0);
+        Persona p3 = new Persona("Felipe", 17, "123456789", 1.75, 78.0);
 
         System.out.println("Es mayor de edad? " + p3.esMayorDeEdad());
         System.out.println(p3);
